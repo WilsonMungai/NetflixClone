3f4310ecda12431cad5ecdd82e9d118d722c771a
WilsonMungai <wilsonmuguthi@gmail.com>
2023-02-18 18:26:35 -0500

    getting started

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Wilson Mungai on 2023-02-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
