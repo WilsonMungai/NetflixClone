efc215af37efc8d2a265e0ae6cc8946c56db268d
WilsonMungai <wilsonmuguthi@gmail.com>
2023-02-23 11:08:59 -0500

    accessing preview view from item click in top search view

--- NetflixClone/Controllers/Core/SearchViewController.swift
+++ NetflixClone/Controllers/Core/SearchViewController.swift
@@ -97,6 +97,36 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
+    // selected movie at index
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // deselct row
+        tableView.deselectRow(at: indexPath, animated: true)
+        // unwrap optionals
+        guard let title = titles[indexPath.row].title ?? titles[indexPath.row].original_title else { return }
+        guard let overView = titles[indexPath.row].overview else { return }
+        
+        // API call
+        APICaller.shared.getMovie(with: title) { [weak self] result in
+            switch result {
+                // success when we have a video
+            case .success(let video):
+                
+                // perform in the main thread
+                DispatchQueue.main.async {
+                    // create a reference for the preview controller
+                    let vc = TitlePreviewViewController()
+                    // configure view controller with the view model
+                    vc.configure(with: TitlePreviewViewModel(title: title, youtubeiew: video, titleOverview: overView))
+                    // push vc
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                }
+                
+                // failure print error
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
 // search conformance
