5fd22064bd244476bbce98b6e040518fb6461133
WilsonMungai <wilsonmuguthi@gmail.com>
2023-02-23 12:54:07 -0500

    fetching and deleting data from the database

--- NetflixClone/Controllers/Core/DownloadsViewController.swift
+++ NetflixClone/Controllers/Core/DownloadsViewController.swift
@@ -9,9 +9,97 @@ import UIKit
 
 class DownloadsViewController: UIViewController {
 
+    // initialize property of type items saved in database
+    private var titles: [TitleItem] = [TitleItem]()
+    // table view
+    private let downloadedTable: UITableView = {
+        let table = UITableView()
+//        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground    
+        title = "Downloads"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        view.backgroundColor = .systemBackground
+        
+        downloadedTable.delegate = self
+        downloadedTable.dataSource = self
+        
+        view.addSubview(downloadedTable)
+        fetchStoredData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        downloadedTable.frame = view.bounds
+    }
+    
+    // MARK: - Private functions
+    private func fetchStoredData() {
+        print("fetching")
+        DataPersistenceManager.shared.fetchingTitlesFromDatabase { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.downloadedTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+// MARK: - Extension
+extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
+    // data source functio
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let count = titles.count
+//        print("Epiosdes are: \(count)")
+        return count
+    }
+
+    // delegate function
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell() }
+//        cell.textLabel?.text = upComing[indexPath.row].original_title ?? upComing[indexPath.row].title ?? "Unkown"
+        let upComingTitles = titles[indexPath.row].original_title ?? titles[indexPath.row].title ?? "Unkown"
+        // unwrapping image view
+        guard let upComingPoster = titles[indexPath.row].poster_path else { return UITableViewCell() }
+        cell.configure(with: TitleViewModel(titleName: upComingTitles , posterURL: upComingPoster))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    // asks the database to delete or insert an item
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // switch on editing style
+        switch editingStyle {
+        case .delete:
+            // delete from the database
+            DataPersistenceManager.shared.deletetitle(model: titles[indexPath.row]) { result in
+                switch result {
+                case .success():
+                    print("Deleted from database")
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+            // remove the item that has been deleted from the array
+            titles.remove(at: indexPath.row)
+            // delete the row
+            downloadedTable.deleteRows(at: [indexPath], with: .fade)
+        default: break
+        }
     }
 }
--- NetflixClone/Managers/DataPersistenceManager.swift
+++ NetflixClone/Managers/DataPersistenceManager.swift
@@ -13,6 +13,8 @@ import CoreData
 class DataPersistenceManager {
     enum DataBaseError: Error {
         case failedToSaveData
+        case failedToRetrieveData
+        case failedToDeleteData
     }
     // singleton
     static let shared = DataPersistenceManager()
@@ -55,6 +57,48 @@ class DataPersistenceManager {
             // error
         } catch {
             completion(.failure(DataBaseError.failedToSaveData))
+        }
+    }
+    
+    // fetching the data
+    // when this function is called it will return an arrey of the objects saved in coredata or an error
+    func fetchingTitlesFromDatabase(completion: @escaping (Result<[TitleItem], Error>)-> Void) {
+        // instance/reference to app delegate
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        // property to hold database
+        let context  = appDelegate.persistentContainer.viewContext
+        
+        // make request to fetch the data
+        let request: NSFetchRequest<TitleItem>
+        
+        request = TitleItem.fetchRequest()
+        
+        do {
+            // asking the context to request the data from the database with my request of the title items
+            let titles = try context.fetch(request)
+            completion(.success(titles))
+        } catch {
+            completion(.failure(DataBaseError.failedToRetrieveData))
+        }
+    }
+    
+    // delete method
+    func deletetitle(model: TitleItem, completion: @escaping (Result<Void, Error>) -> Void) {
+        // instance/reference to app delegate
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        // property to hold database
+        let context  = appDelegate.persistentContainer.viewContext
+        
+        // asking context to delete item
+        context.delete(model)
+        
+        do {
+            try context.save()
+            completion(.success(()))
+        } catch {
+            completion(.failure(DataBaseError.failedToDeleteData))
         }
     }
 }
